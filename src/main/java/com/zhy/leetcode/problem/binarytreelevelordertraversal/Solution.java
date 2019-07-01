22f4a3dc2089a8ec8a4dfc69b5c031da9cdcb3df
haoyan.zhang <haoyan.zhang@qunar.com>
2019-07-01 20:06:20 +0800

    1

---
+++ src/main/java/com/zhy/leetcode/problem/binarytreelevelordertraversal/Solution.java
@@ -0,0 +1,26 @@
+package com.zhy.leetcode.problem.binarytreelevelordertraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        travel(root, 0, result);
+        return result;
+    }
+
+    private void travel(TreeNode node, int deep, List<List<Integer>> result) {
+        if (node == null) {
+            return;
+        }
+        while (result.size() <= deep) {
+            result.add(new ArrayList<>());
+        }
+        result.get(deep).add(node.val);
+        travel(node.left, deep + 1, result);
+        travel(node.right, deep + 1, result);
+    }
+}
